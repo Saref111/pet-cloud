c70d5cde047c1370bd8cffde0caf6b93e67dc1ed
Saref111 <saref012@gmail.com>
2021-09-08 18:38:07 +0300

    added reducers and store

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './reducers/index'
+import { Provider} from 'react-redux';
 import App from './App';
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
